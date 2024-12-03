f61326336cb002f9350838906408fa7b2081f1c1
FrancescoGiommi <francescogiommi97@icloud.com>
2024-12-03 13:33:46 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+//! Esercizio
+/* 
+Creare un semplice form con un campo input per il titolo di un articolo del blog.
+
+Al submit del form, mostrare la lista degli articoli inseriti.
+
+Infine dare la possibilità di cancellare ciascun articolo utilizzando un'icona.
+
+BONUS
+
+    Implementare la funzionalità di modifica del titolo di un post.
+    Aggiungere più campi al form (ad es. lo stato di un articolo - draft, published - o l’autore) */
+
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <></>;
+}
+
+export default App;
